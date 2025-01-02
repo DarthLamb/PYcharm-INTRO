b7a735f8f3f564aea7e08719396baccd56238b97
DarthLamb <nicholasnuni@outlook.com>
2025-01-02 14:22:36 -0500

    g

---
+++ bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'max', 'heck']
+print(bicycles)
+print("Starting first and last!")
+print(bicycles[0].title())
+print(bicycles[-1].title())
+print("Starting the for-each loop!")
+for x in bicycles:
+    print(x)
---
+++ tiy11_greetings.py
@@ -0,0 +1,3 @@
+friends = ["Zack" , "Brian"]
+print(f"Hey {friends[0]}! ")
+print(f"What is up {friends[1]}?")
