06a17a12f0e5bbcee320d2b12bb52e56d75b8655
DarthLamb <nicholasnuni@outlook.com>
2025-01-11 22:35:35 -0500

    I finished my Binary search algorithm, with the inclusion of a "verify" method

--- IntroToAlgo/binary_search.py
+++ IntroToAlgo/binary_search.py
@@ -1,2 +1,22 @@
 def binary_search(list, target):
-    return None
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first + last) // 2
+        if list[midpoint] < target:
+            first = midpoint + 1
+        elif list[midpoint] > target:
+            last = midpoint - 1
+        elif list[midpoint] == target:
+            return midpoint
+
+def verify(index):
+    if index is not None:
+        print("Your target is in the list at:", index)
+    else:
+        print("Your target is not in the list")
+
+numbers = [1,2,3,4,5,6,7,8]
+result = binary_search(numbers,1)
+verify(result)
