5f18ae621b78dfef9dba3524faf01a50be88b802
bobmo <bob.monday@outlook.com>
2025-01-16 11:36:47 -0500

    g

--- Examples/first_numbers.py
+++ Examples/first_numbers.py
@@ -1,2 +1,9 @@
-for value in range(1,5):
-    print(value)
+# for value in range(1,5):
+#     print(value)
+
+my_list = list(range(10))
+print(my_list)
+
+print(min(my_list))
+print(max(my_list))
+print(sum(my_list))
