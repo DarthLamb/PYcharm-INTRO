5fe069d76bb01b78de41da9c7d5f60b677ab8d7b
DarthLamb <nicholasnuni@outlook.com>
2025-01-28 09:35:50 -0500

    g

--- Classes/car.py
+++ Classes/car.py
@@ -24,22 +24,22 @@ class Car:
         """Add a given amount to the odometer reading"""
         self.odometer_reading += miles
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
-
-"""Modifying an attributes value directly"""
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
-"""Modifying an attribute through a method"""
-my_new_car.update_odometer(26)
-my_new_car.read_odometer()
-my_new_car.update_odometer(22)
-"""Incrementing an attribute value through a method"""
-my_second_car = Car('subaru', 'outback', 2020)
-print(my_second_car.get_descriptive_name())#"""Get descriptive name is pretty important function"""
-my_second_car.update_odometer(20_324)
-my_second_car.read_odometer()
-my_second_car.increment_odometer(100)
-my_second_car.read_odometer()
+# my_new_car = Car('audi', 'a4', 2019)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
+#
+# """Modifying an attributes value directly"""
+# my_new_car.odometer_reading = 23
+# my_new_car.read_odometer()
+# """Modifying an attribute through a method""" b vvvv  b
+# my_new_car.update_odometer(26)
+# my_new_car.read_odometer()
+# my_new_car.update_odometer(22)
+# """Incrementing an attribute value through a method"""
+# my_second_car = Car('subaru', 'outback', 2020)
+# print(my_second_car.get_descriptive_name())#"""Get descriptive name is pretty important function"""
+# my_second_car.update_odometer(20_324)
+# my_second_car.read_odometer()
+# my_second_car.increment_odometer(100)
+# my_second_car.read_odometer()
 
